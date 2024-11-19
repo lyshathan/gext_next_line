48641b64c8c4454e0479e88f218898039d14448f
lyshathan <lysha.than@gmail.com>
2024-11-19 18:31:40 +0100

    MODIF : push from home

--- get_next_line.c
+++ get_next_line.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   get_next_line.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: lthan <lthan@student.42.fr>                +#+  +:+       +#+        */
+/*   By: ly-sha <ly-sha@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/19 12:28:48 by lthan             #+#    #+#             */
-/*   Updated: 2024/11/19 15:01:49 by lthan            ###   ########.fr       */
+/*   Updated: 2024/11/19 18:19:39 by ly-sha           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -46,20 +46,22 @@ char	*set_line(const char *memory)
 	size_t	i;
 	char	*line;
 
-	len = 0;
-	while (memory[len] && memory[len] != '\n')
-		len++;
-	printf("len = %zu\n", len);
-	line = calloc(len + 1, sizeof(char));
-	// if (!line)
-	// 	return (NULL);						//Have to free everything?
 	i = 0;
-	while (i < len)
+	while (memory[i] && memory[i] != '\n')
+		i++;
+	if (ft_strchr(memory, '\n'))
+		i++;
+//	printf("i = %zu\n", i);
+	line = calloc(i + 1, sizeof(char));
+	if (!line)
+		return (NULL);						//Have to free anything?
+	i = 0;
+	while (memory[i] && memory[i] != '\n')
 	{
 		line[i] = memory[i];
 		i++;
 	}
-	if (memory[len] == '\n')
+	if (ft_strchr(memory, '\n'))
 		line[i] = '\n';
 	return (line);
 }
@@ -70,30 +72,24 @@ char	*reset_memory(char *memory)
 	size_t	i;
 	size_t	j;
 
-	temp = ft_strdup(memory);
+	i = 0;
+	while (memory[i] && memory[i] != '\n')
+		i++;
+	if (ft_strchr(memory, '\n'))
+		i++;
+	temp = calloc((ft_strlen(memory) - i + 1), sizeof(char));
+	if (!temp)
+	{
+		free(memory);
+		memory = NULL;
+		return (NULL);
+	}
+	j = 0;
+	while (memory[i])
+		temp[j++] = memory[i++];
 	free(memory);
 	memory = NULL;
-	i = 0;
-	while (temp[i] && temp[i] != '\n')
-		i++;
-	if (temp[i] == '\n')
-		i++;
-	memory = calloc((ft_strlen(temp) - i + 1), sizeof(char));
-	if (!memory)
-	{
-		free(temp);					//Have to free anything?
-		temp = NULL;
-		return (NULL);
-	}					
-	j = 0;
-	while (temp[i])
-	{
-		memory[j] = temp[i];
-		j++;
-		i++;
-	}
-	free(temp);
-	return (memory);
+	return (temp);
 }
 
 char	*get_next_line(int fd)
@@ -103,6 +99,8 @@ char	*get_next_line(int fd)
 	char		*line;
 	int		red;
 
+	if (fd <= 0 || BUFFER_SIZE <= 0)
+		return (NULL);
 	line = NULL;
 	buffer = ft_calloc((BUFFER_SIZE + 1), sizeof(char));
 	if (!buffer)
@@ -114,7 +112,7 @@ char	*get_next_line(int fd)
 		free(memory);
 		return (NULL);
 	}
-	printf("memory = |%s|\n", memory);
+//	printf("memory = |%s|\n", memory);
 	line = set_line(memory);
 	memory = reset_memory(memory);
 //	printf("reset memory = |%s|\n", memory);
